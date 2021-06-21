83da55f4c7a253cc4df86b7ae6e4e94e2f84dc7d
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-06-21 19:20:07 -0300

    configuracion controller
    se configuró el proyecto con sus respectivas dependencias para poder trabajar con la estructura MVC, se deja un controller y dos jsp de ejemplo

---
+++ Banco/src/main/java/controller/PaginaController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PaginaController {
+	
+	@RequestMapping("inicio.html")
+	public ModelAndView inicio() {
+		ModelAndView MV = new ModelAndView();
+		MV.setViewName("Pagina1");
+		return MV;
+	}
+
+	@RequestMapping("redireccionar_pag2.html")
+	public ModelAndView eventoRedirPag2() {
+		ModelAndView MV = new ModelAndView();
+		MV.setViewName("Pagina2");
+		return MV;
+	}
+	
+}
---
+++ Banco/src/main/webapp/WEB-INF/vistas/Pagina1.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+Esta es la pagina 1
+
+<form action="redireccionar_pag2.html" method="get">
+
+<input type="submit" value="Redirigir pag 2" name="btnRedirigir">
+
+</form>
+
+</body>
+</html>
