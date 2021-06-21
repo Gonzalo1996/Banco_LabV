522848cef2b6f66562b1f833482e29aca1fe992d
Mauricio <Mauricio@LAPTOP-NED4MI9Q>
2021-06-21 11:25:13 -0300

    Se agrego la tabla usuario con las anotaciones de hibernate correspondientes y su relacion uno a uno.

---
+++ Banco/src/main/java/dominio/Usuario.java
@@ -0,0 +1,88 @@
+package dominio;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Usuario 
+{
+	@Id
+	private Integer idUsuario;
+	private String contrasenia;
+	private Integer tipoUsuario;
+	private String nombreUsuario;
+	private Boolean estado;
+	
+	@OneToOne(cascade = {CascadeType.ALL})
+	@JoinColumn(name="dni")
+	private Cliente cliente;
+	
+	public Usuario() {
+	}
+	
+	public Usuario(Integer idUsuario, Cliente cliente, String contrasenia, Integer tipoUsuario, String nombreUsuario, Boolean estado) {
+		super();
+		this.idUsuario = idUsuario;
+		this.cliente = cliente;
+		this.contrasenia = contrasenia;
+		this.tipoUsuario = tipoUsuario;
+		this.nombreUsuario = nombreUsuario;
+		this.estado = estado;
+	}
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public Integer getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(Integer tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [cliente=" + cliente + "]";
+	}
+}
