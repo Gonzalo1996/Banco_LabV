fb3e8b68c74c193957ff5c925500b57af31a082b
Albertstone98 <69548242+Albertstone98@users.noreply.github.com>
2021-07-06 16:12:46 -0300

    .

--- Banco/src/main/java/banco/dao/CuentaDAOImpl.java
+++ Banco/src/main/java/banco/dao/CuentaDAOImpl.java
@@ -101,10 +101,6 @@ public class CuentaDAOImpl implements CuentaDAO{
 	public List<Cuenta> obtenerCuentasPorCliente(Integer c_dni) {
 		Session session = this.sessionFactory.getCurrentSession();
 		session.beginTransaction();
-		/*String q = "SELECT u"
-				+ " FROM Usuario as u JOIN u.cliente JOIN u.tipoUsuario "
-				+ "WHERE u.cliente.dni = :dni AND u.contrasenia = :contrasenia AND u.nombreUsuario= :nombreUsuario";*/
-		
 		String hql = "SELECT c FROM Cuenta as c JOIN c.cliente WHERE c.cliente.dni = :c_dni";
 		Query query = session.createQuery(hql);
 		query.setParameter("c_dni", c_dni);
