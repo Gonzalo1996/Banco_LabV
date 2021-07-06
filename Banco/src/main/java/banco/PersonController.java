fdf177015b3375806e298b329d16c3e0e56a912d
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-07-05 21:17:42 -0300

    constantes de tipoUsuario y tipoCuenta

--- Banco/src/main/java/banco/PersonController.java
+++ Banco/src/main/java/banco/PersonController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import banco.helpers.Constantes;
 import banco.model.Cliente;
 import banco.model.Cuenta;
 import banco.model.Person;
@@ -149,7 +150,7 @@ public class PersonController {
 			
 			model.addAttribute("usuario", usuario.getCliente().getNombre());
 			
-			if(usuario.getTipoUsuario().getId() == 1) {
+			if (usuario.getTipoUsuario().getId() == Constantes.TipoUsuario.ADMIN.id) {
 				return "redirect:/listadoCuentas.html";
 			}
 			else {
